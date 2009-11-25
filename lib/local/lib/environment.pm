ad97dd6f25e6e68be22809157e6744ad7067ecdc
Christopher Mckay <potatohead@potatolan.com>
2009-11-25 17:02:47 +1000

    Added a perl make environment and upgrade environment
    2 commands, 1 to make a new perl environment, and 1 to upgrade an existing local
    lib to a full environment. A full environment differs in that it contains an
    activate script

--- lib/local/lib/environment.pm
+++ lib/local/lib/environment.pm
@@ -1,11 +1,17 @@
 package local::lib::environment;
 
-use warnings;
 use strict;
+use warnings;
+use Params::Validate qw/validate
+                        SCALAR/;
+use local::lib;
+use File::Path;
+use autodie;
+use Carp;
 
 =head1 NAME
 
-local::lib::environment - The great new local::lib::environment!
+local::lib::environment - module catchphrase
 
 =head1 VERSION
 
@@ -18,77 +24,133 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+describe the module, working code example
 
-Perhaps a little code snippet.
+=cut
 
-    use local::lib::environment;
+=head1 DESCRIPTION
 
-    my $foo = local::lib::environment->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=cut
 
 =head1 FUNCTIONS
 
-=head2 function1
+=cut
+
+=head2 make_environment
+
+Creates a new local lib environment in the supplied directory
 
 =cut
 
-sub function1 {
-}
+sub make_environment {
+    my %args = validate(@_,
+        {
+            directory => {
+                type => SCALAR
+            }
+        }
+    );
+    
+    # create base local lib
+    local::lib->ensure_dir_structure_for($args{directory});
 
-=head2 function2
+    # then upgrade it to a local lib environment
+    upgrade_to_environment($args{directory});
+
+} # end of subroutine make_environment
+
+
+=head2 upgrade_to_environment
+
+Upgrades a local lib directory into a full environment
 
 =cut
 
-sub function2 {
+sub upgrade_to_environment {
+    my %args = validate(@_,
+        {
+            directory => {
+                type => SCALAR
+            }
+        }
+    );
+
+    File::Path::mkpath($args{directory} . '/bin');
+
+    open (my $activate_fh, '>', $args{directory} . '/bin/lle_activate');
+
+    print $activate_fh, q| 
+# This file must be used with "source bin/activate" *from bash*
+# you cannot run it directly
+
+deactivate () {
+    if [ -n "$_OLD_VIRTUAL_PATH" ] ; then
+        PATH="$_OLD_VIRTUAL_PATH"
+        export PATH
+        unset _OLD_VIRTUAL_PATH
+    fi
+
+    # This should detect bash and zsh, which have a hash command that must
+    # be called to get it to forget past commands.  Without forgetting
+    # past commands the $PATH changes we made may not be respected
+    if [ -n "$BASH" -o -n "$ZSH_VERSION" ] ; then
+        hash -r
+    fi
+
+    if [ -n "$_OLD_VIRTUAL_PS1" ] ; then
+        PS1="$_OLD_VIRTUAL_PS1"
+        export PS1
+        unset _OLD_VIRTUAL_PS1
+    fi
+
+    unset VIRTUAL_ENV
+    if [ ! "$1" = "nondestructive" ] ; then
+    # Self destruct!
+        unset deactivate
+    fi
 }
+
+# unset irrelavent variables
+deactivate nondestructive
+
+VIRTUAL_ENV="/home/potato/.virtualenvs/test"
+export VIRTUAL_ENV
+
+_OLD_VIRTUAL_PATH="$PATH"
+PATH="$VIRTUAL_ENV/bin:$PATH"
+export PATH
+
+_OLD_VIRTUAL_PS1="$PS1"
+if [ "`basename \"$VIRTUAL_ENV\"`" = "__" ] ; then
+    # special case for Aspen magic directories
+    # see http://www.zetadev.com/software/aspen/
+    PS1="[`basename \`dirname \"$VIRTUAL_ENV\"\``] $PS1"
+else
+    PS1="(`basename \"$VIRTUAL_ENV\"`)$PS1"
+fi
+export PS1
+
+# This should detect bash and zsh, which have a hash command that must
+# be called to get it to forget past commands.  Without forgetting
+# past commands the $PATH changes we made may not be respected
+if [ -n "$BASH" -o -n "$ZSH_VERSION" ] ; then
+    hash -r
+fi|
+
+    close $activate_fh;
+} # end of subroutine upgrade_to_environment
+
+
 
 =head1 AUTHOR
 
-Christopher Mckay, C<< <potatohead at potatolan.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-local-lib-environment at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=local-lib-environment>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
+Christopher Mckay (cmckay), C<< <potatohead@potatolan.com> >>
 
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
-    perldoc local::lib::environment
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=local-lib-environment>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/local-lib-environment>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/local-lib-environment>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/local-lib-environment/>
-
-=back
+perldoc local::lib::environment
 
 
 =head1 ACKNOWLEDGEMENTS
@@ -108,3 +170,5 @@ See http://dev.perl.org/licenses/ for more information.
 =cut
 
 1; # End of local::lib::environment
+
+
